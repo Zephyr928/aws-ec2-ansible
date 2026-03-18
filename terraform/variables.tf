330b43145d8ef18d6af0d53b1d8483b787a63cf3
Zephyr928 <58963989+Zephyr928@users.noreply.github.com>
2026-03-18 10:00:15 -0500

    Update variables.tf
    Configured variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,23 @@
-variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 2
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for Amazon Linux 2"
+  type        = string
+  default     = "ami-02dfbd4ff395f2a1b"  # This is the AMI for Amazon Linux
+}
